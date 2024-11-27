d7ad500569da3fcc501d9e2db76779e43c04b406
Stefan Lier <lier.stefan@gmail.com>
2024-11-27 16:46:34 +0100

    feat: use more implicit way to define seeded images to control href for pictures

--- utils/db/seed.ts
+++ utils/db/seed.ts
@@ -1,38 +1,30 @@
 import 'dotenv/config';
 import {drizzle} from 'drizzle-orm/node-postgres';
-import {reset, seed} from "drizzle-seed";
+import {reset} from "drizzle-seed";
+import {faker} from '@faker-js/faker'
 import * as schema from '../../src/config/schema';
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 async function main() {
-    const randomObjectIds: string[] = []
-    for (let i = 0; i < 100; i++) {
-        randomObjectIds.push(crypto.randomUUID().toString());
-    }
-
     await reset(db, schema);
-    await seed(db, schema).refine((funcs) => ({
-            images: {
-                count: 100,
-                columns: {
-                    objectId: funcs.valuesFromArray({values: randomObjectIds})
-                },
-                with: {
-                    dimensions: 10
-                }
-            },
-            dimensions: {
-                columns:
-                    {
-                        width: funcs.int({minValue: 1024, maxValue: 2048, isUnique: false}),
-                        height: funcs.int({minValue: 768, maxValue: 1536, isUnique: false}),
-                        name: funcs.string({isUnique: false}),
-                        href: funcs.string({isUnique: false})
-                    }
-            }
-        })
-    );
-}
 
+    for (let i = 0; i < 100; i++) {
+        const queryResult = await db.insert(schema.images).values({
+            objectId: crypto.randomUUID().toString(),
+        }).returning();
+
+        const createdImage = queryResult[0];
+        const width =faker.number.int({ min: 1024, max: 2048 });
+        const height = faker.number.int({ min: 768, max: 1536 });
+
+        await db.insert(schema.dimensions).values({
+            imageId: createdImage.id,
+            width: width,
+            height: height,
+            name: faker.word.sample(),
+            href: `https://picsum.photos/seed/${createdImage.objectId}/${width}/${height}`,
+        });
+    }
+}
 main();
